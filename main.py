1e9d2cd28aa29d55efcd8ca8d51b65077244a6e7
EvgeniyCW <168594490+EvgeniyCWA@users.noreply.github.com>
2024-06-04 18:30:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-gas_price = 5
-eth_price = 3100
+gas_price = 1
+eth_price = 30
 print(f"Стоимость газа {gas_price} gwei")
-transaction_price = 21000 * gas_price / 10 ** 9
-print(f"Стоимость транзакции 21000 gas * {gas_price} gwei = {transaction_price} ETH или {transaction_price * eth_price} $")
+transaction_price = 22000 * gas_price / 10 ** 1
+print(f"Стоимость транзакции 22000 gas * {gas_price} gwei = {transaction_price} ETH или {transaction_price * eth_price} $")
 print("Для получения дропа нужно сделать 100 транзакций")
 print(f"Расход для получения дропа составит {gas_price} gwei = {transaction_price * 100} ETH")
